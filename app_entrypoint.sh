c8c5d8a46cae9eac029c08a280bae72d6137d8e5
speakerBOXdev <joshuawrose@gmail.com>
2018-09-06 09:54:17 -0400

    Initial commit of spargo/web

---
+++ app_entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## The purpose of this file is to configure the container in
+## preparation for the /run.sh command.
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## The purpose of this file is to run the executable that
+## perpetuates the container.
+
+httpd -DFOREGROUND

0bdd2d95c922dc3f96bee7b580a094ef06d7bb16
speakerBOXdev <joshuawrose@gmail.com>
2018-10-16 07:47:20 -0400

    Run scripts in /spargo/scripts directory if exists.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [ -d /spargo/scripts ]
+then
+  echo Running scripts in scripts directory
+  chmod 555 /spargo/scripts/*.sh
+  for filename in /spargo/scripts/*.sh; do
+    eval "$filename"
+  done
+fi
+
 # Run Apache HTTPD
 httpd -DFOREGROUND

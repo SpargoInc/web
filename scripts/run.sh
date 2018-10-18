a1aba715ee0cdba9e755e62e7a06fd89dbbc7700
speakerBOXdev <joshuawrose@gmail.com>
2018-10-18 12:38:25 -0400

    Log scripts that are executing. Omit '00-' scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -d /spargo/scripts ]
 then
   echo Running scripts in scripts directory
   chmod 555 /spargo/scripts/*.sh
-  for filename in /spargo/scripts/*.sh; do
+  files=`ls /spargo/scripts/*.sh | grep -v /spargo/scripts/00-`
+  for filename in $files; do
+    echo "========================"
+    echo "==== RUN $filename"
     eval "$filename"
   done
 fi
